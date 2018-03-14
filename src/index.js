07a7ab1bbe3685a6d2e925f05cd2520b972dbc49
Benfarhat <benfarhat.elyes@gmail.com>
2018-03-14 14:10:26 +0100

    Redux

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-/*
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-*/
-
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import redux  from 'redux'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker'
+import App from './App'
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-
-/*
-  const client = new ApolloClient({
-    //link: new HttpLink({ uri: 'https://localhost:3000/graphql' }),
-    link: new HttpLink(),
-    cache: new InMemoryCache()
-  });
-
-  const RootComponent = () => (
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  );
-  */
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
